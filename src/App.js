dfe32e87cebdd8c8ab4a4a836446aa8544efb87c
gongmeishan <maoyao_1992@hotmail.com>
2018-03-30 10:51:30 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EasyABC from './EasyABC'
-
+//input everything to dom
 class App extends Component {
   render() {
     return (
